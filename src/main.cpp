ab46aca2faab6ec8e0139c498a6e9326a9e58146
Vincent <skriosdreys@gmail.com>
2019-10-25 15:01:16 +0200

    Base code for the window and background setup as well as classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,41 @@
 #include <SFML/Graphics.hpp>
 #include <Box2D/Box2D.h>
 #include <globals.h>
+#include <iostream>
 
 int main()
 {
+	sf::RenderWindow window(sf::VideoMode(800, 647), "Platformer");
+	window.setVerticalSyncEnabled(true);
+
+	sf::Texture backgroundTexture;
+	sf::Sprite backgroundSprite;
+
+	if(!backgroundTexture.loadFromFile("data/background.png"))
+	{
+		std::cerr << "[Error]\n";
+	}
+	backgroundSprite.setTexture(backgroundTexture);
+
+	sf::Event event;
+	while(window.pollEvent(event))
+	{
+		if (event.type == sf::Event::Closed)
+		{
+			window.close();
+		}
+	}
+
+	window.clear(sf::Color::White);
+
+	window.draw(backgroundSprite);
+	
+	window.display();
+
+
+
+
+	
 	system("pause");
 	return 0;
 }
